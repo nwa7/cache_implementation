47b0a1884b34918bf19e1cc62a113731b480e039
Elia <eliaroard@gmail.com>
2023-11-30 11:24:10 +0100

    added comment on cache and started on main

--- cache_impl.h
+++ cache_impl.h
@@ -14,30 +14,33 @@
 #ifndef _CACHE_IMPL_H_
 #define _CACHE_IMPL_H_
 
-#define WORD_SIZE_BYTE                   4
-#define DEFAULT_CACHE_SIZE_BYTE          32
-#define DEFAULT_CACHE_BLOCK_SIZE_BYTE    8
-#define DEFAULT_CACHE_ASSOC              2   /* This can be changed to 1(for direct mapped cache) or 4(for fully assoc cache) */
-#define DEFAULT_MEMORY_SIZE_WORD         128    
-#define CACHE_ACCESS_CYCLE               1
-#define MEMORY_ACCESS_CYCLE              100
-#define CACHE_SET_SIZE                   ((DEFAULT_CACHE_SIZE_BYTE)/(DEFAULT_CACHE_BLOCK_SIZE_BYTE*DEFAULT_CACHE_ASSOC))
+#define WORD_SIZE_BYTE 4
+#define DEFAULT_CACHE_SIZE_BYTE 32
+#define DEFAULT_CACHE_BLOCK_SIZE_BYTE 8
+#define DEFAULT_CACHE_ASSOC                                                    \
+  2 /* This can be changed to 1(for direct mapped cache) or 4(for fully assoc  \
+       cache) */
+#define DEFAULT_MEMORY_SIZE_WORD 128
+#define CACHE_ACCESS_CYCLE 1
+#define MEMORY_ACCESS_CYCLE 100
+#define CACHE_SET_SIZE                                                         \
+  ((DEFAULT_CACHE_SIZE_BYTE) /                                                 \
+   (DEFAULT_CACHE_BLOCK_SIZE_BYTE * DEFAULT_CACHE_ASSOC))
 
 /* Function Prototypes */
 void init_memory_content();
 void init_cache_content();
 void print_cache_entries();
-int check_cache_data_hit(void* addr, char type);
+int check_cache_data_hit(void *addr, char type);
 int access_memory(void *addr, char type);
-
 
 /* Cache Entry Structure */
 typedef struct cache_entry {
-    int valid;
-    int tag;
-    int timestamp;
-    char data[DEFAULT_CACHE_BLOCK_SIZE_BYTE];
+  int valid;
+  int tag;
+  int timestamp; /* for approximate LRU: it should be equal to data access so
+                    far */
+  char data[DEFAULT_CACHE_BLOCK_SIZE_BYTE];
 } cache_entry_t;
-
 
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 /*
- *
  * main.c
  *
  * 20493-01 Computer Architecture
@@ -10,8 +9,8 @@
  *
  */
 
-#include <stdio.h>
 #include "cache_impl.h"
+#include <stdio.h>
 
 int num_cache_hits = 0;
 int num_cache_misses = 0;
@@ -22,49 +21,54 @@ int num_access_cycles = 0;
 int global_timestamp = 0;
 
 int retrieve_data(void *addr, char data_type) {
-    int value_returned = -1; /* accessed data */
+  int value_returned = -1; /* accessed data */
 
-    /* Invoke check_cache_data_hit() */
+  /* Invoke check_cache_data_hit() */
 
+  /* In case of the cache miss event, access the main memory by invoking
+   * access_memory() */
 
-    
-    /* In case of the cache miss event, access the main memory by invoking access_memory() */
-
-
-
-    return value_returned;    
+  return value_returned;
 }
 
 int main(void) {
-    FILE *ifp = NULL, *ofp = NULL;
-    unsigned long int access_addr; /* byte address (located at 1st column) in "access_input.txt" */
-    char access_type; /* 'b'(byte), 'h'(halfword), or 'w'(word) (located at 2nd column) in "access_input.txt" */
-    int accessed_data; /* This is the data that you want to retrieve first from cache, and then from memory */ 
-    
-    init_memory_content();
-    init_cache_content();
-    
-    ifp = fopen("access_input.txt", "r");
-    if (ifp == NULL) {
-        printf("Can't open input file\n");
-        return -1;
-    }
-    ofp = fopen("access_output.txt", "w");
-    if (ofp == NULL) {
-        printf("Can't open output file\n");
-        fclose(ifp);
-        return -1;
-    }
-    
-    /* Fill out here by invoking retrieve_data() */
+  FILE *ifp = NULL, *ofp = NULL;
+  unsigned long int access_addr; /* byte address (located at 1st column) in
+                                    "access_input.txt" */
+  char access_type;  /* 'b'(byte), 'h'(halfword), or 'w'(word) (located at 2nd
+                        column) in "access_input.txt" */
+  int accessed_data; /* This is the data that you want to retrieve first from
+                        cache, and then from memory */
 
+  init_memory_content(); // 1) Invoke init_memory_content
+  init_cache_content();  // 2) Invoke init_cache_content
 
-
-
-
+  ifp = fopen("access_input.txt", "r"); // 3) open access_input.txt file
+  if (ifp == NULL) {
+    printf("Can't open input file\n");
+    return -1;
+  }
+  ofp = fopen("access_output.txt", "w");
+  if (ofp == NULL) {
+    printf("Can't open output file\n");
     fclose(ifp);
-    fclose(ofp);
-    
-    print_cache_entries();
-    return 0;
+    return -1;
+  }
+
+  char line[256]; // to store the line that is being processed
+  while (fgets(line, sizeof(line), ifp) !=
+         NULL) { // need to read all the input data
+    /* Fill out here by invoking retrieve_data() */
+    // call retrieve data to 4) Read each line
+
+    fprintf(ofp, "Accessed data: %d\n", accessed_data);
+    global_timestamp++;
+    /
+  }
+
+  fclose(ifp);
+  fclose(ofp);
+
+  print_cache_entries();
+  return 0;
 }
